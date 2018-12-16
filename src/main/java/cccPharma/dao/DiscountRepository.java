0c8335cbf5f787a8641fba1d44a4ade91c9bf3ec
Gabriel Felipe <gabriel.gomes@ccc.ufcg.edu.br>
2018-12-15 21:00:58 -0300

    Update DiscountRepository.java

--- src/main/java/cccPharma/dao/DiscountRepository.java
+++ src/main/java/cccPharma/dao/DiscountRepository.java
@@ -6,5 +6,4 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface DiscountRepository extends CrudRepository<Discount, Long> {
 
-    List<Discount> findByDescription(String description);
 }
